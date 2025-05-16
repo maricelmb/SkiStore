20a00b4fcd813063ea27fdd3232b055533eeb8ea
maricel <maricelmb@gmail.com>
2025-05-16 16:13:48 +1000

    S2.13: Added db thru EF

---
+++ SkiStore/Data/StoreContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SkiStore.Entities;
+
+namespace SkiStore.Data
+{
+    public class StoreContext(DbContextOptions options) : DbContext(options)
+    {
+        public required DbSet<Product> Products { get; set; }
+    }
+}
--- SkiStore/Program.cs
+++ SkiStore/Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using SkiStore.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<StoreContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
