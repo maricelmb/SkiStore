c83bf472ac78f2963d0ebe079ccb15d339da4e55
maricel <maricelmb@gmail.com>
2025-05-16 16:53:17 +1000

    S2.14: Seed data to db

---
+++ SkiStore/Controllers/ProductsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SkiStore.Controllers
+{
+    [Route("api/[controller]")] //http://localhost:5001/api/products
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+    }
+}
