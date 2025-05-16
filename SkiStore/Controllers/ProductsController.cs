b08ad68bcfdf786e5f09ffc44d2071032bdd78d9
maricel <maricelmb@gmail.com>
2025-05-16 17:16:45 +1000

    S2.15: Added a product controller

--- SkiStore/Controllers/ProductsController.cs
+++ SkiStore/Controllers/ProductsController.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SkiStore.Data;
+using SkiStore.Entities;
 
 namespace SkiStore.Controllers
 {
     [Route("api/[controller]")] //http://localhost:5001/api/products
     [ApiController]
-    public class ProductsController : ControllerBase
+    public class ProductsController(StoreContext context) : ControllerBase
     {
+        [HttpGet]
+        public ActionResult<List<Product>> GetProducts()
+        { 
+            return context.Products.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Product> GetProduct(int id) 
+        {
+            var product = context.Products.Find(id);
+
+            if (product == null) return NotFound(); 
+
+            return product;
+        }
     }
 }
