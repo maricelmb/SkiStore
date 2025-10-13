f884db799ac7dd1d65d4ae6968308dfc1a935b64
maricel <maricelmb@gmail.com>
2025-10-13 15:11:47 +1100

    Create a basket and basketItem to table to store the cart values

---
+++ SkiStore/Entities/Basket.cs
@@ -0,0 +1,54 @@
+﻿namespace SkiStore.Entities
+{
+    public class Basket
+    {
+        public int Id { get; set; }
+
+        //stored in cookie/local storage on client
+        public required string BasketId { get; set; }
+
+        public List<BasketItem> Items { get; set; } = [];
+
+        #region Tracking the state of the item and has nothing to do with the db yet
+        public void AddItem(Product product, int quantity)
+        {
+            if (product == null) ArgumentNullException.ThrowIfNull(product);
+
+            if (quantity <= 0) throw new ArgumentOutOfRangeException("Quantity should be greater than 0.", nameof(quantity));
+
+            var existingItem = FindItem(product.Id);
+
+            if (existingItem == null)
+            {
+                Items.Add(new BasketItem
+                {
+                    ProductId = product.Id,
+                    Product = product,
+                    Quantity = quantity
+                });
+            }
+            else
+            {
+                existingItem.Quantity += quantity;
+            }
+        }
+
+        public void RemoveItem(int productId, int quantity)
+        {
+            if (quantity <= 0) throw new ArgumentOutOfRangeException("Quantity should be greater than 0.", nameof(quantity));
+
+            var item = FindItem(productId);
+
+            if (item == null) return;
+            item.Quantity -= quantity;
+
+            if (item.Quantity <= 0) Items.Remove(item);
+        }
+
+        private BasketItem? FindItem(int id)
+        {
+            return Items.FirstOrDefault(item => item.ProductId == id);
+        } 
+        #endregion
+    }
+}
---
+++ SkiStore/Entities/BasketItem.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SkiStore.Entities
+{
+    [Table("BasketItems")] //Table name in the db
+    public class BasketItem
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+
+        //navigation properties
+        public int ProductId { get; set; }
+        public required Product Product { get; set; }
+
+        //navigation back to the basket
+        //makes sure that if we delete the basket, all its items are deleted too (onDelete: Cascade)
+        public int BasketId { get; set; }
+
+        //can't use 'required' keyword here as it will require us to provide a value if Id in Product.AddItem
+        // null! - null forgiving operator - suppresses warning w/o making it nullable
+        public Basket Basket { get; set; } = null!;
+    }
+}
