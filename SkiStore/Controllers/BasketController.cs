3547de0fef9183bf94e2913dde50d34da7f3ec12
maricel <maricelmb@gmail.com>
2025-10-18 23:46:08 +1100

    Added a basket controller and an api to get all the items from the basket

---
+++ SkiStore/Controllers/BasketController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SkiStore.Data;
+using SkiStore.Entities;
+
+
+namespace SkiStore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasketController(StoreContext context) : BaseApiController
+    {
+        [HttpGet]
+        public async Task<ActionResult<Basket>> GetBasket()
+        {
+           var basket = await context.Baskets
+                .Include(x => x.Items)
+                .ThenInclude(x => x.Product)
+                .FirstOrDefaultAsync(x => x.BasketId == Request.Cookies["basketId"]);
+
+            if(basket == null) return NotFound();
+
+            return basket;
+        }
+    }
+}
